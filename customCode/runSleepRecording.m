c2a527374a68206a30d2c3996eee0dfe2a2a2dfe
pedgomgal1 <pgomez-ibis@us.es>
2023-10-05 11:05:25 +0100

    give option to crop the whole recorded image

--- customCode/runSleepRecording.m
+++ customCode/runSleepRecording.m
@@ -18,6 +18,32 @@ counter=1;
 folder2save=[date '_' num2str(hourToStartAcquisition) '_' num2str(minToStartAcquisition)];
 mkdir(fullfile(rootFolder,folder2save));
 currentClock=clock;
+
+splitOrSelectDir = questdlg('Do you want to crop the frame?','Selection','Yes','No','Yes');
+
+if strcmp(splitOrSelectDir,'Yes')
+    happyDecision = 0;
+    while happyDecision==0
+        %select initial ROI
+        frame = getsnapshot(v);
+        fig1 = imshow(frame);
+        hold on
+        title('Select ROI - Click at UPPER-LEFT corner, then at LOWER-RIGHT corner')
+        [ROI(1,1), ROI(1,3)] = ginput(1);
+        p(1) = plot([ROI(1,1) ROI(1,1)], [1 size(frame,1)], '-r');
+        p(2) = plot([1 size(frame,2)], [ROI(1,3) ROI(1,3)], '-r');
+        [ROI(1,2), ROI(1,4)] = ginput(1);
+        plot([ROI(1,1) ROI(1,2) ROI(1,2) ROI(1,1) ROI(1,1)], ...
+                [ROI(1,3) ROI(1,3) ROI(1,4) ROI(1,4) ROI(1,3)],'LineWidth',2);
+        rightROI = questdlg('Right ROI selection?','Selection','Yes','No','Yes');
+        if strcmp(rightROI,'Yes')
+            happyDecision=1; 
+            close all
+        end
+    end
+end
+
+
 while currentClock(4)<hourToStartAcquisition || currentClock(5)< minToStartAcquisition
    currentClock=clock; 
 end
@@ -27,8 +53,13 @@ clockToStartExp = currentClock;
 while etime(clock,clockToStartExp) < totalRecordingsSeconds
     currentClock=clock; 
     frame = getsnapshot(v);
-    %% imwrite(frame,fullfile(rootFolder,folder2save,['Image_' num2str(counter) '_' num2str(currentClock(4)) 'h' num2str(currentClock(5)) 'm' num2str(floor(currentClock(6))) 's.tif']));
-    imwrite(frame,fullfile(rootFolder,folder2save,'StackSleep.tif'), 'WriteMode' , 'append') ;
+    if strcmp(splitOrSelectDir,'Yes')
+        frameCropped = imcrop(frame,[ROI(1,[1,3]),ROI(1,2)-ROI(1,1),ROI(1,4)-ROI(1,3)]);
+        imwrite(frameCropped,fullfile(rootFolder,folder2save,'StackSleep.tif'), 'WriteMode' , 'append') ;
+    else
+        imwrite(frame,fullfile(rootFolder,folder2save,'StackSleep.tif'), 'WriteMode' , 'append') ;
+    end
+    %% save image stack
     counter=counter+1;
     pause(acquistionImageTime-etime(clock,currentClock));
 end
