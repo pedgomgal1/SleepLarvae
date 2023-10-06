f4f98a9a0f34b191d960da39befb7fb397eea195
pedgomgal1 <pgomez-ibis@us.es>
2023-10-06 18:37:04 +0100

    including larva size thresholding

--- customCode/src/countBoutsPerHour.m
+++ customCode/src/countBoutsPerHour.m
@@ -1,21 +1,20 @@
 function countBoutsPerHour(directoryROIs)
     
     thresholdDiffPixelsValue = 10;
+    maxLarvaArea = 200; %no larva area larger than 200 pixels
+    minLarvaArea = 15;
     numberOfPixelsThreshold = 5;
     pixels2CheckFromCentroid=45;
     nImagesPerHour=600;
 
-    folderROIdir = dir(fullfile(directoryROIs,'**','ROI_*'));
+    folderROIdir = dir(fullfile(directoryROIs,'**','ROI_*.tif'));
 
-   parfor nROIFolders = 1:size(folderROIdir,1)
-
-        dirROI = fullfile(directoryROIs,folderROIdir(nROIFolders).name);
-        allROIdir = dir(fullfile(dirROI,'Image_*'));
-        
-        disp(['Running analysis: ' folderROIdir(nROIFolders).folder '-' folderROIdir(nROIFolders).name])
-%         if ~exist(fullfile(dirROI,'boutsData','boutsPerHour.mat'),'file')
-            saveLarvaMovement(allROIdir,thresholdDiffPixelsValue,numberOfPixelsThreshold,pixels2CheckFromCentroid,nImagesPerHour)
-%         end
+    parfor nROIFolders = 1:size(folderROIdir,1)
+        pathROI_tif = fullfile(folderROIdir(nROIFolders).folder,folderROIdir(nROIFolders).name);
+           
+        disp(['Running analysis: ' pathROI_tif])
+        %if ~exist(fullfile(dirROI,'boutsData','boutsPerHour.mat'),'file')
+          saveLarvaMovement(pathROI_tif,thresholdDiffPixelsValue,numberOfPixelsThreshold,minLarvaArea,maxLarvaArea,pixels2CheckFromCentroid,nImagesPerHour);
+        %end
     end
-
 end
