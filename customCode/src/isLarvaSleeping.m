4d75edb75c91c378adb1893d422367fce9cdcb3e
pedgomgal1 <pgomez-ibis@us.es>
2023-10-06 18:35:35 +0100

    updating the larva tracking

--- customCode/src/isLarvaSleeping.m
+++ customCode/src/isLarvaSleeping.m
@@ -1,10 +1,11 @@
-function [boolMovement,restImageFinal,nPixels,Centroid2Check,larvaFilt] = isLarvaSleeping(imageROI1, imageROI2,imageBackground,thresholdPixelsValue,numberOfPixelsThreshold,pixels2CheckFromCentroid,CentroidPrevious,larvaFiltPrevious)
+function [boolMovement,restImageFinal,nPixels,Centroid2Check,larvaFilt] = isLarvaSleeping(imageROI1, imageROI2,imageBackground,thresholdPixelsValue,numberOfPixelsThreshold,pixels2CheckFromCentroid,CentroidPrevious,larvaFiltPrevious,minLarvaArea,maxLarvaArea)
     
+    % Create a disk mask seeding in the larvaFilt centroid
     maskCentroid=false(size(imageROI1));
     maskCentroid(round(CentroidPrevious(2)),round(CentroidPrevious(1)))=1;
     dilatedCentroidPrevious=imdilate(maskCentroid,strel('disk',pixels2CheckFromCentroid));
-    dilatedCentroid = dilatedCentroidPrevious;
 
+    % Get the XOR larvae pixels
     imageROI1minusBG = abs(imageBackground-imageROI1);
     imageROI2minusBG = abs(imageBackground-imageROI2);
     restImage12 = abs(imageROI1minusBG-imageROI2minusBG)>thresholdPixelsValue;
@@ -12,9 +13,14 @@ function [boolMovement,restImageFinal,nPixels,Centroid2Check,larvaFilt] = isLarv
     restCombined=(restImage12 | restImage21);
     restImageFinal = (restCombined.*dilatedCentroidPrevious)>0;
 
-    larvaFilt = bwareafilt((imageROI2minusBG>thresholdPixelsValue)>0 & dilatedCentroidPrevious,1);
-    Centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
+    intersectLarv = (abs(imageBackground-imageROI1)>thresholdPixelsValue) & (abs(imageBackground-imageROI2)>thresholdPixelsValue) | restImage21;
 
+    % update centroid based on NOT pixels in Frame 2, else based on XOR
+    % pixels of Frames 1-2. If not possible, then
+
+    % filter larva body of frame 2, and keep the centroid if no other centroid can be captured from restImageFinal or restCombined
+    larvaFilt = bwareafilt((imageROI2minusBG>thresholdPixelsValue)>0 & dilatedCentroidPrevious,[minLarvaArea,maxLarvaArea]);
+    Centroid2Check=struct2array(regionprops(bwareafilt(larvaFilt,1),'Centroid'));
     [y,x]=find(restImageFinal);
     if ~isempty(x)
         Centroid2Check = [round(mean(x)),round(mean(y))];
@@ -24,50 +30,40 @@ function [boolMovement,restImageFinal,nPixels,Centroid2Check,larvaFilt] = isLarv
         labelLarva = bwlabel(imageROI2minusBG>thresholdPixelsValue);
         labels=labelLarva(restImage21);
         selectedBlobLarva = (labelLarva == mode(labels(labels>0)));
-        larvaFilt = bwareafilt(selectedBlobLarva & dilatedCentroid,1);
+        larvaFilt = bwareafilt(selectedBlobLarva & dilatedCentroid,[minLarvaArea,maxLarvaArea]);
+        larvaFilt = bwareafilt(larvaFilt,1);
+        if ~any(larvaFilt(:)>0)
+            %Larva intersection
+            larvaFilt = bwareafilt(bwareafilt(intersectLarv,[minLarvaArea,maxLarvaArea]),1);
+        end
     else
-        if any(restCombined(:)>0) && ~any(restCombined(:) & larvaFilt(:)) && ~any(larvaFiltPrevious(:) & larvaFilt(:))
-            [y,x]=find(restCombined);
-            Centroid2Check = [round(mean(x)),round(mean(y))];
-            maskCentroid=false(size(imageROI1));
-            maskCentroid(round(Centroid2Check(2)),round(Centroid2Check(1)))=1;
-            dilatedCentroid=imdilate(maskCentroid,strel('disk',pixels2CheckFromCentroid));
-            labelLarva = bwlabel(imageROI2minusBG>thresholdPixelsValue);
-            labels=labelLarva(restImage21);
-            selectedBlobLarva = (labelLarva == mode(labels(labels>0)));
-            larvaFilt = bwareafilt(selectedBlobLarva & dilatedCentroid,1);
-            Centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
-        end
-    end
-
-    if sum(larvaFilt(:))==0
-        larvaFilt = bwareafilt(((imageROI2minusBG>thresholdPixelsValue).*dilatedCentroid)>0,1);
-        if sum(larvaFilt(:))==0
-            larvaFilt = bwareafilt((imageROI2minusBG>thresholdPixelsValue)>0,1);
-        end
-        Centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
-
-        if isempty(Centroid2Check) && ~isempty(x)
+        larvaFilt = bwareafilt(bwareafilt(intersectLarv,[minLarvaArea,maxLarvaArea]),1);
+        if any(larvaFilt(:)>0)
+            [y,x]=find(larvaFilt);
             Centroid2Check = [round(mean(x)),round(mean(y))];
         end
     end
+    
+    %update centroid 
     maskCentroid=false(size(imageROI1));
     maskCentroid(round(Centroid2Check(2)),round(Centroid2Check(1)))=1;
     dilatedCentroid=imdilate(maskCentroid,strel('disk',pixels2CheckFromCentroid));
 
     if  ~any(sum((dilatedCentroidPrevious & dilatedCentroid)>0))
         dilatedCentroid=dilatedCentroidPrevious;
-        larvaFilt = bwareafilt(((imageROI2minusBG>thresholdPixelsValue).*dilatedCentroid)>0,1);
+        larvaFilt = bwareafilt(bwareafilt((imageROI2minusBG>thresholdPixelsValue)>0 & dilatedCentroid,[minLarvaArea,maxLarvaArea]),1);
         Centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
         if isempty(Centroid2Check)
             Centroid2Check=CentroidPrevious;
         end
     end
 
+
+    %check movement and amount of movement
     restImageFinal = (restCombined.*dilatedCentroid)>0;
     nPixels=sum(restImageFinal(:));
     boolMovement = nPixels>numberOfPixelsThreshold;
     
-%     imshow([restImageFinal,imageROI2minusBG>10,restCombined,larvaFilt])
+    %imshow([restImageFinal,imageROI2minusBG>10,restCombined,larvaFilt])
 
 end
--- customCode/src/saveLarvaMovement.m
+++ customCode/src/saveLarvaMovement.m
@@ -1,116 +1,124 @@
-function saveLarvaMovement(allROIdir,thresholdDiffPixelsValue,numberOfPixelsThreshold,pixels2CheckFromCentroid,nImagesPerHour)
-%            try
-                nameImages={allROIdir.name};
-                onlyNames=cellfun(@(x) strsplit(x,'_'),nameImages,'UniformOutput',false);
-                numImages=cellfun(@(x) str2num(x{2}),onlyNames);
-                [~,indx]=sort(numImages);
-                orderROIDir = allROIdir(indx,:);
-                
-                imageBackground=detectBackground(orderROIDir(1:nImagesPerHour,:));
-                
-                larva1 = abs(imageBackground-imread(fullfile(orderROIDir(1).folder,orderROIDir(1).name)))>thresholdDiffPixelsValue;
-                larva2 = abs(imageBackground-imread(fullfile(orderROIDir(2).folder,orderROIDir(2).name)))>thresholdDiffPixelsValue;
-                labelLarva = bwlabel(larva1);
-                labels=labelLarva(abs(larva2-larva1)>0);
-                larvaFilt = (labelLarva == mode(labels(labels>0)));
-                centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
+function saveLarvaMovement(fileName,thresholdDiffPixelsValue,numberOfPixelsThreshold,minLarvaArea,maxLarvaArea,pixels2CheckFromCentroid,nImagesPerHour)
+
+        % Extract the folder path from the input file name
+        [folderPath, ~, ~] = fileparts(fileName);
         
-                arrayBouts=[];
-                arrayPixels=[];
-                
-                mkdir(fullfile(orderROIDir(1).folder,'binaryLarva'));
-                mkdir(fullfile(orderROIDir(1).folder,'sequenceConcat'));
-                mkdir(fullfile(orderROIDir(1).folder,'boutsData'));
-                imwrite(larvaFilt,fullfile(orderROIDir(1).folder,'binaryLarva',[num2str(1) '.jpg']));
+        % Get information about the input images (e.g size)
+        infoImage = imfinfo(fileName);
+
+        % Detect initial background to be subtracted
+        imageBackground = detectBackground(fileName, 1:nImagesPerHour);
         
-                cellBouts=cell(round(size(orderROIDir,1)/nImagesPerHour),4);
-                counterNan=0;
-                while counterNan<100
-                    for nTempImg = 1:size(orderROIDir,1)-1
+        % Initialize variables for larva tracking
+        larva1 = abs(imageBackground - imread(fileName, 1)) > thresholdDiffPixelsValue;
+        larva2 = abs(imageBackground - imread(fileName, 2)) > thresholdDiffPixelsValue;
+        % Use bwareafilt to keep objects within the specified area range
+        larva1 = bwareafilt(larva1, [minLarvaArea,maxLarvaArea]);
+        larva2 = bwareafilt(larva2, [minLarvaArea,maxLarvaArea]);
+
+        % Detect the larva position to do not consider noisy regions far
+        % from larva position
+        labelLarva = bwlabel(larva1);
+        labels = labelLarva(abs(larva2 - larva1) > 0);
+        if ~any(labels)
+            larvaFilt = labelLarva == mode(labelLarva(larva2));
+        else
+            larvaFilt = labelLarva == mode(labels(labels > 0));
+        end
+        centroid2Check = struct2array(regionprops(larvaFilt, 'Centroid'));
+
+         % Initialize arrays to store tracking data
+        arrayBouts = [];
+        arrayPixels = [];
+        
+        % Create directories for storing results
+        mkdir(fullfile(folderPath, 'binaryLarva'));
+        mkdir(fullfile(folderPath, 'boutsData'));
+
+        % Save the initial larva filter as an image
+        imwrite(larvaFilt, fullfile(folderPath, 'binaryLarva', [num2str(1) '.jpg']));
+        
+        % Initialize a cell array to store tracking data per hour
+        cellBouts = cell(round(size(infoImage, 1) / nImagesPerHour), 4);
+        counterNan = 0;
+                
+
+        %% Detect larva position across the whole experiment
+        while counterNan<100
+            for nTempImg = 1:size(infoImage,1)-1
+                
+                % Read two consecutive frames
+                img1 = imread(fileName, nTempImg);
+                img2 = imread(fileName, nTempImg + 1);
+                
+                % Update the background and centroid every hour
+                if mod(nTempImg, nImagesPerHour) == 0 && (nTempImg + nImagesPerHour) < size(infoImage, 1)
+                    imageBackground = detectBackground(fileName, nTempImg:nTempImg + nImagesPerHour);
+                    larva1 = abs(imageBackground - img1) > thresholdDiffPixelsValue;
+                    larvaFilt = bwareafilt(larva1, [minLarvaArea,maxLarvaArea]);
+                    centroid2Check = struct2array(regionprops(bwareafilt(larvaFilt,1), 'Centroid'));
+                end
+    
+                try
+                     % Check if larva is moving
+                    [isMoving, difImage, nPixels, centroid2Check, larvaFilt] = isLarvaSleeping(img1, img2, imageBackground, thresholdDiffPixelsValue, numberOfPixelsThreshold, pixels2CheckFromCentroid, centroid2Check, larvaFilt,minLarvaArea,maxLarvaArea);
+                    
+                    % Save the filtered larva image with additional information
+                    imwrite(larvaFilt, fullfile(folderPath, 'binaryLarva', [num2str(nTempImg + 1) '_' num2str(nPixels) 'px.jpg']))
+                catch
+                    try
+                        if nTempImg > nImagesPerHour / 2
+                            imageBackground = detectBackground(fileName, (nTempImg - round(nImagesPerHour / 2)):(nTempImg + round(nImagesPerHour / 2)));
+                        else
+                            imageBackground = detectBackground(fileName, 1:nTempImg + round(nImagesPerHour / 2));
+                        end
+                        larva1 = abs(imageBackground - img1) > thresholdDiffPixelsValue;
+                        larvaFilt = bwareafilt(larva1, 1);
+                        if sum(centroid2Check) == 0
+                            centroid2Check = struct2array(regionprops(larvaFilt, 'Centroid'));
+                        end
+                        [isMoving, difImage, nPixels, centroid2Check, larvaFilt] = isLarvaSleeping(img1, img2, imageBackground, thresholdDiffPixelsValue, numberOfPixelsThreshold, pixels2CheckFromCentroid, centroid2Check, larvaFilt,minLarvaArea,maxLarvaArea);
                         
-                        img1=imread(fullfile(orderROIDir(nTempImg).folder,orderROIDir(nTempImg).name));
-                        img2=imread(fullfile(orderROIDir(nTempImg+1).folder,orderROIDir(nTempImg+1).name));
+                        % Save the filtered larva image with additional information
+                        imwrite(larvaFilt, fullfile(folderPath, 'binaryLarva', [num2str(nTempImg + 1) '_' num2str(nPixels) 'px.jpg']))
+
+                    catch
+                        % Handle exceptions when larva tracking fails
+                        isMoving = NaN;
+                        nPixels = NaN;
+                        counterNan = counterNan + 1;
+                        imwrite(ones(size(larvaFilt)), fullfile(folderPath, 'binaryLarva', [num2str(nTempImg + 1) '_NaN.jpg']));
                         
-                        imwrite([img1,img2],fullfile(orderROIDir(nTempImg).folder,'sequenceConcat',[num2str(nTempImg) '_' num2str(nTempImg+1) '.png']))
-            
-                        %%NEXT CODE COULD BE COMMENTED IF THE RESNET
-                        %%CLASSIFIER WORKS PROPERLY
-                        
-%                         %%update background just in case some changes appear
-%                         if mod(nTempImg,nImagesPerHour)==0 && (nTempImg+nImagesPerHour)<size(orderROIDir,1)
-%                                 imageBackground=detectBackground(orderROIDir(nTempImg:nTempImg+nImagesPerHour,:));
-%                                 larva1 = abs(imageBackground-img1)>thresholdDiffPixelsValue;
-%                                 larvaFilt=bwareafilt(larva1,1);
-%                                 centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
-%                         end
-%             
-%                         try
-%                             if sum(centroid2Check)==0
-%                                 centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
-%                             end
-%                             [isMoving,difImage,nPixels,centroid2Check,larvaFilt] = isLarvaSleeping(img1,img2,imageBackground,thresholdDiffPixelsValue,numberOfPixelsThreshold,pixels2CheckFromCentroid,centroid2Check,larvaFilt);
-%             
-%                             imwrite(larvaFilt,fullfile(orderROIDir(nTempImg).folder,'binaryLarva',[num2str(nTempImg+1) '_' num2str(nPixels) 'px.jpg']))
-%                         catch
-%                             try
-%                                 if nTempImg>nImagesPerHour/2
-%                                     imageBackground=detectBackground(orderROIDir(nTempImg-round(nImagesPerHour/2):nTempImg+round(nImagesPerHour/2),:));
-%                                 else
-%                                     imageBackground=detectBackground(orderROIDir(1:nTempImg+round(nImagesPerHour/2),:));
-%                                 end
-%                                 larva1 = abs(imageBackground-img1)>thresholdDiffPixelsValue;
-%                                 larvaFilt=bwareafilt(larva1,1);
-%                                 if sum(centroid2Check)==0
-%                                     centroid2Check=struct2array(regionprops(larvaFilt,'Centroid'));
-%                                 end                                
-%                                 [isMoving,difImage,nPixels,centroid2Check,larvaFilt] = isLarvaSleeping(img1,img2,imageBackground,thresholdDiffPixelsValue,numberOfPixelsThreshold,pixels2CheckFromCentroid,centroid2Check,larvaFilt);
-%                                 imwrite(larvaFilt,fullfile(orderROIDir(nTempImg).folder,'binaryLarva',[num2str(nTempImg+1) '_' num2str(nPixels) 'px.jpg']))
-%                             catch
-%                                 isMoving=NaN;
-%                                 nPixels=NaN;
-%                                 counterNan=counterNan+1;
-%                                 imwrite(ones(size(larvaFilt)),fullfile(orderROIDir(nTempImg).folder,'binaryLarva',[num2str(nTempImg+1) '_NaN.jpg']))
-%                                 if counterNan==100
-%                                     disp(['Too many NaN in: ' allROIdir(1).folder])
-%                                     break;
-%                                 end
-%                             end
-%                         end
-%                         
-%                         
-%                         
-% %                         imgChange = img2;
-% %                         imgChange(difImage)=255;
-% %                         
-% %                         imshow([img1,img2,imageBackground,imgChange,uint8(larvaFilt).*255])
-% %                         title([strrep(orderROIDir(nTempImg+1).name,'_',' ') ' is moving? ' num2str(isMoving) ' , ' num2str(nPixels) ' pixels'])
-% %                         hold on
-%                         % close all
-%             
-%                         %Save bouts data per hour
-%                         arrayBouts(end+1)=isMoving;
-%                         arrayPixels(end+1)=nPixels;
-%                         if mod(nTempImg,nImagesPerHour)==0
-%                             nHour=round(nTempImg/nImagesPerHour);
-%                             cellBouts(nHour,1:4)={nHour,arrayBouts,arrayPixels,sum(isnan(arrayBouts))};
-%                             arrayBouts=[];
-%                             arrayPixels=[];
-%                             save(fullfile(orderROIDir(1).folder,'boutsData','boutsPerHour.mat'),'cellBouts')
-%                         end
-% 
-%                         if nTempImg==size(orderROIDir,1)-1
-%                             nHour=round(nTempImg/nImagesPerHour);
-%                             cellBouts(nHour,1:4)={nHour,arrayBouts,arrayPixels,sum(isnan(arrayBouts))};
-%                             cellBouts=cell2table(cellBouts,VariableNames={'hour','bouts','pixelsMove','numNaNs'});
-%                             
-%                             save(fullfile(orderROIDir(1).folder,'boutsData','boutsPerHour.mat'),'cellBouts')
-%                             disp([allROIdir(1).folder ' number of NaNs images: ' num2str(sum(cellBouts.numNaNs))])
-%                             counterNan=100;
-%                         end
-            
+                        % Exit loop if too many exceptions occur
+                        if counterNan == 100
+                            disp(['Too many NaN in: ' folderPath])
+                            break;
+                        end
                     end
                 end
-% %             catch
-% %                     disp(['Error in: ' allROIdir(1).folder])
-% %             end
+                          
+                %Save bouts data per hour
+                arrayBouts(end+1)=isMoving;
+                arrayPixels(end+1)=nPixels;
+                if mod(nTempImg,nImagesPerHour)==0
+                    nHour=round(nTempImg/nImagesPerHour);
+                    cellBouts(nHour,1:4)={nHour,arrayBouts,arrayPixels,sum(isnan(arrayBouts))};
+                    arrayBouts=[];
+                    arrayPixels=[];
+                    save(fullfile(folderPath,'boutsData','boutsPerHour.mat'),'cellBouts')
+                end
+
+                if nTempImg==size(infoImage,1)-1
+                    nHour=round(nTempImg/nImagesPerHour);
+                    cellBouts(nHour,1:4)={nHour,arrayBouts,arrayPixels,sum(isnan(arrayBouts))};
+                    cellBouts=cell2table(cellBouts,VariableNames={'hour','bouts','pixelsMove','numNaNs'});
+                    
+                    save(fullfile(folderPath,'boutsData','boutsPerHour.mat'),'cellBouts')
+                    disp([folderPath ' number of NaNs images: ' num2str(sum(cellBouts.numNaNs))])
+                    counterNan=100;
+                end
+    
+            end
+        end
+
 end
