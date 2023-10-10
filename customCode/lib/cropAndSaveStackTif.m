7fdce168c6b0443d48cc6a88315130ab0e9f11e2
pedgomgal1 <pgomez-ibis@us.es>
2023-10-10 18:22:14 +0100

    sort error in cropping

--- customCode/lib/cropAndSaveStackTif.m
+++ customCode/lib/cropAndSaveStackTif.m
@@ -5,7 +5,7 @@ function cropAndSaveStackTif(fileName,ROI,folder2save,nRoi)
     mkdir(folderRoi)
     for nZ = 1:size(infoImage,1)
         loadedImage = imread(fileName,nZ);
-        imgCropped = imcrop(loadedImage,[ROI(1,3),ROI(2)-ROI(1),ROI(4)-ROI(3)]);
+        imgCropped = imcrop(loadedImage,[ROI(1),ROI(3),ROI(2)-ROI(1),ROI(4)-ROI(3)]);
         imwrite(imgCropped ,fullfile(folderRoi,['ROI_' num2str(nRoi) '.tif']), 'WriteMode' , 'append') ;
     end
 end
